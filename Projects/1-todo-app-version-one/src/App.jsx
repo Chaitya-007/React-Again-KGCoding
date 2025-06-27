cdd6b6f1cab4c222d99ca3f126483612b9e0059a
Chaitanya <chaitanyaasole@gmail.com>
2025-06-27 19:53:57 +0530

    Integration BootStrap UI

--- Projects/1-todo-app-version-one/src/App.jsx
+++ Projects/1-todo-app-version-one/src/App.jsx
@@ -1,8 +1,24 @@
-function App()
-{
-  return <center class='todo-container'>
-    <h1>Todo App</h1>
-  </center>
+function App() {
+  return (
+    <center class="todo-container">
+      <h1>Todo App</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter todo here..." />
+          </div>
+          <div class="col-4">
+            <input type="date" name="" id="" />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
 export default App;
