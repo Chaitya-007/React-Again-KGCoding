3ba4619dd5c5d296a8354439348aaf937f4160e3
Chaitanya <chaitanyaasole@gmail.com>
2025-06-27 20:05:08 +0530

    Made TODO Project

--- Projects/1-todo-app-version-one/src/App.jsx
+++ Projects/1-todo-app-version-one/src/App.jsx
@@ -16,6 +16,34 @@ function App() {
             </button>
           </div>
         </div>
+
+        <div class="row">
+          <div class="col-6">
+            Buy Milk
+          </div>
+          <div class="col-4">
+            4/10/2023
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6">
+            Go to College
+          </div>
+          <div class="col-4">
+            4/10/2023
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
       </div>
     </center>
   );
