55b68a475bf0fd0dd216575985ff169a01542113
Chaitanya <chaitanyaasole@gmail.com>
2025-07-16 17:00:32 +0530

    Added React.Fragment

--- 18_Fragments/learning-fragment/src/App.jsx
+++ 18_Fragments/learning-fragment/src/App.jsx
@@ -4,13 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <React.Fragment>
-      <h1>Health Food</h1>
+      <h1>Healthy Food</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        <li className="list-group-item">Dal</li>
+        <li className="list-group-item">Rice</li>
+        <li className="list-group-item">Vegetables</li>
+        <li className="list-group-item">Fruits</li>
+        <li className="list-group-item">Nuts</li>
       </ul>
     </React.Fragment>
   );
