031eaa09c484d899dbe771f11687483dcbddd856
Chaitanya <chaitanyaasole@gmail.com>
2025-07-16 17:01:33 +0530

    Refactor App component to use shorthand syntax for React.Fragment

--- 18_Fragments/learning-fragment/src/App.jsx
+++ 18_Fragments/learning-fragment/src/App.jsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <h1>Healthy Food</h1>
       <ul className="list-group">
         <li className="list-group-item">Dal</li>
@@ -12,7 +12,7 @@ function App() {
         <li className="list-group-item">Fruits</li>
         <li className="list-group-item">Nuts</li>
       </ul>
-    </React.Fragment>
+    </>
   );
 }
 
