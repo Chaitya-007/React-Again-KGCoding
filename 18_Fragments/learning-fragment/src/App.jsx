8da409ce249e619036f3e57aef6e1d6209e78d4f
Chaitanya <chaitanyaasole@gmail.com>
2025-07-16 17:30:16 +0530

    2nd way of ternary rendering

--- 18_Fragments/learning-fragment/src/App.jsx
+++ 18_Fragments/learning-fragment/src/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  let foodItems = ["Dal", "Rice", "Vegetables", "Fruits", "Nuts"];
+  // let foodItems = ["Dal", "Rice", "Vegetables", "Fruits", "Nuts"];
 
-  // let foodItems = [];
+  let foodItems = [];
+
+  let emptyMsg =
+    foodItems.length === 0 ? <h3>No food items available</h3> : null;
 
   return (
     <>
       <h1>Healthy Food</h1>
-      {foodItems.length === 0 ? <h3>No food items available</h3> : null}
+      {emptyMsg}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">
