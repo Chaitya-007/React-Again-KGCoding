f525d609c4079feed01478582f2320fdc62906f4
Chaitanya <chaitanyaasole@gmail.com>
2025-07-16 18:26:30 +0530

    Array Destructuring

--- 18_Fragments/learning-fragment/src/components/Item.jsx
+++ 18_Fragments/learning-fragment/src/components/Item.jsx
@@ -1,5 +1,7 @@
 const Item = (props) => {
-  return <li className="list-group-item">{props.foodItem}</li>;
+  let { foodItem } = props;
+
+  return <li className="list-group-item">{foodItem}</li>;
 };
 
 export default Item;
