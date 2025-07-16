695475dc9570fab2cfb5435b74f0fbafba59cb2c
Chaitanya <chaitanyaasole@gmail.com>
2025-07-16 17:34:21 +0530

    Logical Rendering

--- 18_Fragments/learning-fragment/src/App.jsx
+++ 18_Fragments/learning-fragment/src/App.jsx
@@ -2,17 +2,14 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  // let foodItems = ["Dal", "Rice", "Vegetables", "Fruits", "Nuts"];
+  let foodItems = ["Dal", "Rice", "Vegetables", "Fruits", "Nuts"];
 
-  let foodItems = [];
-
-  let emptyMsg =
-    foodItems.length === 0 ? <h3>No food items available</h3> : null;
+  // let foodItems = [];
 
   return (
     <>
       <h1>Healthy Food</h1>
-      {emptyMsg}
+      {foodItems.length === 0 && <h3>No food items available</h3>}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">
