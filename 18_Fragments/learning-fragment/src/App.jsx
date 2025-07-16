bf5818895093efc3a4f0651d511faa670ead565a
Chaitanya <chaitanyaasole@gmail.com>
2025-07-16 16:58:55 +0530

    React Explects to return only a single component each time

--- 18_Fragments/learning-fragment/src/App.jsx
+++ 18_Fragments/learning-fragment/src/App.jsx
@@ -1,6 +1,19 @@
-function App()
-{
-  return <h1>Hello World</h1>
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Health Food</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </React.Fragment>
+  );
 }
 
 export default App;
