283e600fbd2431d40caf46de81005957e42d3951
Chaitanya <chaitanyaasole@gmail.com>
2025-07-16 16:30:04 +0530

    Added bolder class from BootStrap

--- Practice_Projects/Bharat_Clock_1.0/bharat_clock/src/components/LastLine.jsx
+++ Practice_Projects/Bharat_Clock_1.0/bharat_clock/src/components/LastLine.jsx
@@ -2,7 +2,7 @@ function LastLine(){
 
   let time = new Date();
 
-  return <p>This is the current time : {time.toLocaleDateString()}</p>
+  return <p>This is the current time : {time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>
 }
 
 export default LastLine;
