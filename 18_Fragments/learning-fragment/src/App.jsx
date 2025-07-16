aae0748c47467171a461994a5368fdcc60baaf74
Chaitanya <chaitanyaasole@gmail.com>
2025-07-16 17:09:50 +0530

    Learning Map Method

--- 18_Fragments/learning-fragment/src/App.jsx
+++ 18_Fragments/learning-fragment/src/App.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  let foodItems = ["Dal", "Rice", "Vegetables", "Fruits", "Nuts"];
+
   return (
     <>
       <h1>Healthy Food</h1>
       <ul className="list-group">
-        <li className="list-group-item">Dal</li>
-        <li className="list-group-item">Rice</li>
-        <li className="list-group-item">Vegetables</li>
-        <li className="list-group-item">Fruits</li>
-        <li className="list-group-item">Nuts</li>
+        {foodItems.map((item) => (
+          <li className="list-group-item">{item}</li>
+        ))}
       </ul>
     </>
   );
