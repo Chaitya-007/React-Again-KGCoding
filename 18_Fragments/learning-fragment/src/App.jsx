585fe49769423eae65dae9e92fd12d5cb99cae01
Chaitanya <chaitanyaasole@gmail.com>
2025-07-16 22:30:04 +0530

    Learnt module.css

--- 18_Fragments/learning-fragment/src/App.jsx
+++ 18_Fragments/learning-fragment/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import FoodItems from "./components/FoodItems.jsx";
 import ErrorMsg from "./components/ErrorMsg.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   let foodItems = ["Dal", "Rice", "Vegetables", "Fruits", "Nuts"];
@@ -10,7 +11,7 @@ function App() {
 
   return (
     <>
-      <h1>Healthy Food</h1>
+      <h1 className="food-heading">Healthy Food</h1>
       <ErrorMsg items={foodItems}></ErrorMsg>
       <FoodItems items={foodItems}></FoodItems>
     </>
