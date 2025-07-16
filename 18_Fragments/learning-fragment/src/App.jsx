45264ac3ef349c7351966a7ee9cac3613acab3b2
Chaitanya <chaitanyaasole@gmail.com>
2025-07-16 17:25:29 +0530

    Ternary Rendering

--- 18_Fragments/learning-fragment/src/App.jsx
+++ 18_Fragments/learning-fragment/src/App.jsx
@@ -2,17 +2,14 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  // let foodItems = ["Dal", "Rice", "Vegetables", "Fruits", "Nuts"];
+  let foodItems = ["Dal", "Rice", "Vegetables", "Fruits", "Nuts"];
 
-  let foodItems = [];
-
-  if (foodItems.length === 0) {
-    return <h3>No food items available</h3>;
-  }
+  // let foodItems = [];
 
   return (
     <>
       <h1>Healthy Food</h1>
+      {foodItems.length === 0 ? <h3>No food items available</h3> : null}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">
