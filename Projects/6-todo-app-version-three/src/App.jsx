018050dca57ab7c6a95b8ed3adb42438ff2db17d
Chaitanya <chaitanyaasole@gmail.com>
2025-07-23 17:43:22 +0530

    Refactor: Remove old todo app version three files and components
    - Deleted package.json, public assets, CSS, and component files related to the previous version of the todo app.
    - Updated App.jsx to integrate new AddToDo component and manage todo items.
    - Cleaned up unused styles and components to streamline the project structure.

--- Projects/6-todo-app-version-three/src/App.jsx
+++ Projects/6-todo-app-version-three/src/App.jsx
@@ -1,11 +1,18 @@
-import AddTodo from "./components/AddToDo";
+import AddToDo from "./components/AddToDo";
 import AppName from "./components/AppName";
 import Todoitems from "./components/Todoitems";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const initialTodoItems = [
+  // const todoItems = [
+  //   {
+  //     name: "Complete Assignment",
+  //     dueDate: "4/12/2023",
+  //   },
+  // ];
+
+  let [todoItems, setTodoItems] = useState([
     {
       name: "Buy Milk",
       dueDate: "4/10/2023",
@@ -14,18 +21,23 @@ function App() {
       name: "Go to College",
       dueDate: "4/10/2023",
     },
-    {
-      name: "Complete Assignment",
-      dueDate: "4/12/2023",
-    },
-  ];
+  ]);
 
-  const [todoItems, setTodoItems] = useState(initialTodoItems);
+  const addTodo = (name, dueDate) => {
+    let newTodo = [
+      ...todoItems,
+      {
+        name: name,
+        dueDate: dueDate,
+      },
+    ];
+    setTodoItems(newTodo);
+  };
 
   return (
     <center className="todo-container">
       <AppName />
-      <AddTodo />
+      <AddToDo onAdd={addTodo} />
       <Todoitems todoItems={todoItems}></Todoitems>
     </center>
   );
--- Projects/6-todo-app-version-three/src/components/AddTodo.jsx
+++ Projects/6-todo-app-version-three/src/components/AddTodo.jsx
@@ -1,15 +1,49 @@
-function AddTodo() {
+import React, { useState } from "react";
+
+function AddToDo({ onAdd }) {
+  const [todoName, setTodoName] = useState("");
+  const [todoDate, setTodoDate] = useState("");
+
+  const hadleNameChange = (event) => {
+    setTodoName(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setTodoDate(event.target.value);
+  };
+
+  const handleAddButtonClick = (todoName, todoDate) => {
+    onAdd(todoName, todoDate);
+    setTodoName("");
+    setTodoDate("");
+  };
+
   return (
     <div className="container text-center">
       <div className="row kg-row">
         <div className="col-6">
-          <input type="text" placeholder="Enter todo here..." />
+          <input
+            type="text"
+            placeholder="Enter todo here..."
+            onChange={handleNameChange}
+            value={todoName}
+          />
         </div>
         <div className="col-4">
-          <input type="date" name="" id="" />
+          <input
+            type="date"
+            name=""
+            id=""
+            onChange={handleDateChange}
+            value={todoDate}
+          />
         </div>
         <div className="col-2">
-          <button type="button" className="btn btn-success kg-button">
+          <button
+            type="button"
+            className="btn btn-success kg-button"
+            onClick={() => handleAddButtonClick(todoName, todoDate)}
+          >
             Add
           </button>
         </div>
@@ -18,4 +52,4 @@ function AddTodo() {
   );
 }
 
-export default AddTodo;
+export default AddToDo;
